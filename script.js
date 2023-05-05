35591aedb9494162beaf890ce8bb7f99fc3b99a4
Jacob Adams <jacob@oldnogales.com>
2023-05-05 00:37:38 -0700

    add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+let bar = document.querySelector('.bars'),
+navItem = document.querySelector('.nav-items');
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+bar.addEventListener('click', () => {
+    navItem.classList.toggle('active');
 })
